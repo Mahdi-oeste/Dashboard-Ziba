9835c53198e0cd92626b9cb38360cfff369b9697
Mahdi-oeste <amendez@oeste.mx>
2026-06-02 16:07:52 -0600

    Final

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "admin",
-  host: process.env.DB_HOST || "10.207.64.123",
-  database: "ziba",
-  password: "admin",
-  port: 5432
+  user:     process.env.DB_USER     || "admin",
+  host:     process.env.DB_HOST     || "10.207.64.123",
+  database: process.env.DB_NAME     || "ziba",
+  password: process.env.DB_PASSWORD || "admin",
+  port:     parseInt(process.env.DB_PORT || "5432"),
 });
 
 module.exports = pool;
